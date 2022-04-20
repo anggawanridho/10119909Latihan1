b3c7de60a187f98d9703917a51c11a78b82629b6
anggawanridho <anggawanridho@gmail.com>
2022-04-20 10:44:21 +0700

    Initial commit

---
+++ app/src/main/java/com/example/a10119909latihan1/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.a10119909latihan1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/**
+ * NIM                      : 10119909
+ * Nama                     : M. Anggawan Ridho Islami
+ * Kelas                    : IF-10K
+ * Tanggal Pengerjaan       : 20 April 2022
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
